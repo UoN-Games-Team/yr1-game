09519d657d98e9fc2c4b687246c1544e67669c6d
Andy Palmer <alpsquid@gmail.com>
2015-02-02 14:24:48 +0000

    Fixed package type

--- desktop/src/com/djammr/westernknights/desktop/DesktopLauncher.java
+++ desktop/src/com/djammr/westernknights/desktop/DesktopLauncher.java
@@ -2,7 +2,7 @@ package com.djammr.westernknights.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.djammer.westernknights.WesternKnights;
+import com.djammr.westernknights.WesternKnights;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
