02cb0056f1751832aeb59933b5c7541eaf917947
Andy Palmer <alpsquid@gmail.com>
2015-02-02 13:53:59 +0000

    Project setup

---
+++ desktop/src/com/djammr/westernknights/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.djammr.westernknights.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.djammer.westernknights.WesternKnights;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WesternKnights(), config);
+	}
+}
