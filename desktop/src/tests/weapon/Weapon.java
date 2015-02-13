6aa275ea177d73528ab0cb0d1ebdff27caeaa0de
matster2 <matster246@gmail.com>
2015-02-13 19:15:17 +0000

    basic weapon system inc: type, enchantment, quality, passive

--- desktop/src/tests/weapon/Weapon.java
+++ desktop/src/tests/weapon/Weapon.java
@@ -4,28 +4,58 @@ package tests.weapon;
 
 public class Weapon {
 
-
-    private String name;
     private String type;
+    private String enchantment;
+    private String quality;
+    private String name;
+    private String passive;
 
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    //Setters
 
     public void setType(String type) {
         this.type = type;
     }
-
-    public String getName() {
-        return name;
+    
+    public void setEnchantment(String enchantment) {
+        this.enchantment = enchantment;
     }
 
+    public void setQuality(String quality) {
+        this.quality = quality;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setPassive(String passive) {
+        this.passive = passive;
+    }
+    
+    
+    //Getters
     public String getType() {
         return type;
     }
+    
+    public String getEnchantment() {
+        return enchantment;
+    }
+    
+    public String getQuality() {
+        return quality;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getPassive() {
+        return passive;
+    }
 
+    //Output Format
     public String toString() {
-        return name + ", " + type;
+        return name + ", " + type + ", " + quality + ", " + enchantment + ", " + passive;
     }
 }
