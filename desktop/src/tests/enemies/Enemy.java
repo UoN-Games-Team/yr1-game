2fb028e5eded07a152cda8230186b56b042f92da
matster2 <matster246@gmail.com>
2015-02-14 18:53:31 +0000

    Enemy Level Creation Basic

--- desktop/src/tests/enemies/Enemy.java
+++ desktop/src/tests/enemies/Enemy.java
@@ -2,27 +2,50 @@ package tests.enemies;
 
 public class Enemy {
 
+	private String faction;
 	private String type;
-	
+	private int level;
+	private int dmg;
 	
 	//Setters
+	 public void setFaction(String faction) {
+	        this.faction = faction;
+	    }
+	
 	 public void setType(String type) {
 	        this.type = type;
+	    }
+	 
+	 public void setLevel(int level) {
+	        this.level = level;
+	    }
+	 
+	 public void setDmgStat(int dmg) {
+	        this.dmg = dmg;
 	    }
 	
 	
 	
-	
 	//Getters
+	public String getFaction() {
+        return faction;
+    }
+	
 	public String getType() {
         return type;
     }
 	
+	public int getLevel() {
+        return level;
+    }
 	
+	public int getDmgStat() {
+        return dmg;
+    }
 	
-	
+		
 	//Output
 	public String toString() {
-        return type;
+        return "Faction: " + faction + ", Type: " + type + ", Level: " + level + ", Damage: " + dmg;
     }
 }
--- desktop/src/tests/enemies/EnemyFactory.java
+++ desktop/src/tests/enemies/EnemyFactory.java
@@ -7,9 +7,9 @@ public class EnemyFactory {
 	private static Random rand = new Random();
 	
 	
-	public static String createType() {
-        int type = rand.nextInt(4);
-        switch (type) {
+	public static String createFaction() {
+        int faction = rand.nextInt(4);
+        switch (faction) {
             case 0:
                 return "Grunt";
             case 1:
@@ -19,12 +19,60 @@ public class EnemyFactory {
         }
     }
 	
+	public static String createType() {
+        int type = rand.nextInt(4);
+        switch (type) {
+            case 0:
+                return "Light Infantry";
+            case 1:
+                return "Heavy Infantry";
+            default:
+                return "Ranged";
+        }
+    }
+	
+	public static int createLevel() {
+		//get player level
+		int playerLevel = 6;
+		
+		//Create Random Modifier Value
+		int modifyvalue = rand.nextInt(20);
+		int modify;
+		if (modifyvalue <= 3){modify = 0;}
+		else if (modifyvalue <= 8){modify = 1;}
+		else if (modifyvalue <= 13){modify = 2;}
+		else if (modifyvalue <= 16){modify = 3;}
+		else if (modifyvalue <= 18){modify = 4;}
+		else {modify = 5;}
+		
+		//Chance of adding/subtracting modify
+		boolean operator;
+		int operatorvalue = rand.nextInt(3);
+		if (operatorvalue == 0) {operator = false;}
+		else {operator = true;}
+
+		//If false subtract modify
+		if (!operator){ modify = modify - modify*2;}
+		
+		int level = playerLevel + modify;
+		if (level < 0){level = 1;}
+		return level;
+    }
+	
+	public static int createDmgStat() {
+       return 0;
+    }
+	
+	
 	
 	
 	public static Enemy createEnemy() {
 		Enemy enemy = new Enemy();
 		
+		enemy.setFaction(createFaction());
 		enemy.setType(createType());
+		enemy.setLevel(createLevel());
+		enemy.setDmgStat(createDmgStat());
 		
 		return enemy;
 	}
