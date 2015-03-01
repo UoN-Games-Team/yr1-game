652250caad7e698964f9af0b7c0e0ba8264ac704
Andy Palmer <alpsquid@gmail.com>
2015-03-01 19:16:25 +0000

    Added 0.0.3 build

--- core/src/com/djammr/westernknights/WKGame.java
+++ core/src/com/djammr/westernknights/WKGame.java
@@ -18,7 +18,7 @@ import javax.naming.ldap.Control;
  */
 public class WKGame extends Game {
 
-	public static final String VERSION = "0.0.1";
+	public static final String VERSION = "0.0.3";
 	public static final float METERS_TO_PIXELS = 60f;
 	public static final float PIXELS_TO_METERS = 1/METERS_TO_PIXELS;
 	public static final int DEBUG_KEY = Input.Keys.APOSTROPHE;
